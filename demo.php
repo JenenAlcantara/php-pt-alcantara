fa781b68fa561bb6d5265abfe862136c2cf133d5
unknown <alcantarajenen799@gmail>
2024-01-22 18:32:47 -0800

    New file

---
+++ demo.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Simple Form Processing</title>
+</head>
+
+<body>
+    <Form action="form_1.php" method="post">
+     FirstName:
+     <input type="text" name="firstname" required/>
+     <br>
+     <br>
+     LastName:
+     <input type="text" name="address" required/>
+     <br>
+     <br>
+     Email Address:
+     <input type="email" name="email" requires/>
+     <br>
+     <br>
+     Password:
+     <input type="password" name="password" required/>
+     <br>
+     <br>
+     <input type="submit" name="submit" required/>
+     </Form>
+</body>
+</html>
